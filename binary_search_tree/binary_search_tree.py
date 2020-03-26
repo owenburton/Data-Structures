1c90722a699d851e6cd784d86c384d73a5cac271
owenburton <owenburton@users.noreply.github.com>
2020-03-25 18:10:33 -0700

    finished BST v1 passed tests

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -8,44 +8,83 @@ class BinarySearchTree:
     def __init__(self, value):
         self.value = value
         self.left = None
-        self.right = None
+        self.right = None 
+
+    ## PLAN FOR INSERT
+    # create a new instance of BinarySearchTree w/ the new value 
+    # if there's no Queue, then initialize one 
+    # initialize variable current as the queue head
+    # if new value is less than current.value, current becomes current.left
+    # if new val < current.value & current.left==None, then current.left==BinarySearchTree(val) & break
+    # if the new value is greater than current.value, current becomes current.right
+    # if new val > current.value & current.right==None, then current.right==BinarySearchTree(val) & break
 
     # Insert the given value into the tree
-    def insert(self, value):
-        # create a new instance of BinarySearchTree w/ the new value 
-        # if there's no Queue, then initialize one 
-        # initialize variable current as the queue head
-        # if new value is less than current.value, current becomes current.left
-        # if new val < current.value & current.left==None, then current.left==BinarySearchTree(val) & break
-        # if the new value is greater than current.value, current becomes current.right
-        # if new val > current.value & current.right==None, then current.right==BinarySearchTree(val) & break
+    def insert(self, value): 
+        if value < self.value:
+            if self.left==None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+
+        if value >= self.value:
+            if self.right==None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
+        
+    ## PLAN FOR CONTAINS
+    # initialize var current as queue head
+    # if target==current.value, return 1
+    # if current==None, return 0
+    # if target < current.value, current becomes current.left 
+    # if target > current.value, current becomes current.left 
+    # return True if total > than 0 else False
 
     # Return True if the tree contains the value
     # False if it does not
-    def contains(self, target):
-        # initialize var current as queue head
-        # if target==current.value, return 1
-        # if current==None, return 0
-        # if target < current.value, current becomes current.left 
-        # if target > current.value, current becomes current.left 
-        # return True if total > than 0 else False 
+    def contains(self, target): 
+        if self.value==target:
+            return 1
+    
+        return 1 in (self.left.contains(target) if self.left else 0, \
+                        self.right.contains(target) if self.right else 0)
+        
+        
+        
+        
+    ## PLAN FOR GET_MAX
+    # start at the root 
+    # intialize var current as head/root
+    # while current is not None
+    # current = current.right
+    # if current is None, return no max/NA, or maybe just return (do nothing)
+    # else return current.value
 
     # Return the maximum value found in the tree
     def get_max(self):
-        # start at the root 
-        # intialize var current as head/root
-        # while current is not None
-        # current = current.right
-        # if current is None, return no max/NA, or maybe just return (do nothing)
-        # else return current.value
+        if self.right==None:
+            return self.value
+        return self.right.get_max()
+        
+
+    ## PLAN FOR FOR_EACH
+    # initialize current as head 
+    # base case is None, so
+    # if current is None, stop/do nothing/maybe return None 
+    # call the function recursively in the return statement
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        # initialize current as head 
-        # base case is None, so
-        # if current is None, stop/do nothing/maybe return None 
-        # call the function recursively in the return statement 
+        cb(self.value)
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
+        
+        
 
 
     # DAY 2 Project -----------------------
